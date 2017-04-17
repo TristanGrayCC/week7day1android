c160c8a855cebe89344a4aee022370372e9da1c7
Tristan Gray <tristangraycc@gmail.com>
2017-04-17 16:22:38 +0100

    get methods completed

--- MovieRankings/app/src/main/java/example/codeclan/com/movierankings/Movie.java
+++ MovieRankings/app/src/main/java/example/codeclan/com/movierankings/Movie.java
@@ -3,17 +3,29 @@ package example.codeclan.com.movierankings;
 /**
  * Created by user on 17/04/2017.
  */
-
 public class Movie {
-    private String name;
-    private String genre;
-    private int ranking;
+        private String name;
+        private String genre;
+        private int ranking;
 
-    public Movie(String name, String genre, int ranking){
-        
+    public Movie(String name, String genre, int ranking) {
+        this.name = name;
+        this.genre = genre;
+        this.ranking = ranking;
     }
 
     public String getName() {
+        String name = this.name;
         return name;
+    }
+
+    public String getGenre() {
+        String genre = this.genre;
+        return genre;
+    }
+
+    public int getRanking() {
+        int ranking = this.ranking;
+        return ranking;
     }
 }
--- MovieRankings/app/src/test/java/example/codeclan/com/movierankings/MovieTest.java
+++ MovieRankings/app/src/test/java/example/codeclan/com/movierankings/MovieTest.java
@@ -11,8 +11,20 @@ import static junit.framework.Assert.assertEquals;
 public class MovieTest {
 
     @Test
-    public void hasname(){
+    public void hasName(){
         Movie movie = new Movie("Sunshine", "Sci-Fi", 7);
         assertEquals("Sunshine", movie.getName());
+    }
+
+    @Test
+    public void hasGenre(){
+        Movie movie = new Movie("Sunshine", "Sci-Fi", 7);
+        assertEquals("Sci-Fi", movie.getGenre());
+    }
+
+    @Test
+    public void hasRanking(){
+        Movie movie = new Movie("Sunshine", "Sci-Fi", 7);
+        assertEquals(7, movie.getRanking());
     }
 }
